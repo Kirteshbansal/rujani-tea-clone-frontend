ca1383298ec1f87fd4784666d91e6283ef8881cc
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-24 09:48:55 +0530

    Change linkTo key's value

--- src/home/categoryCardData.js
+++ src/home/categoryCardData.js
@@ -4,41 +4,41 @@ export const categoryCardData = [
     name: "AWARD WINNERS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Gold_Tea_Leaf_1000x.jpg?v=1592641524",
-    linkTo: "/collection/award-winners",
+    linkTo: "/collection/1",
   },
   {
     id: 2,
     name: "SIGNATURE TEAS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Black_Pearl_1000x.jpg?v=1592641550",
-    linkTo: "/collection/signature-teas",
+    linkTo: "/collection/2",
   },
   {
     id: 3,
     name: "ASSAM BLACK TEAS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Assamica_Premium_Orthodox_1000x.jpg?v=1592641460",
-    linkTo: "/collection/black-tea",
+    linkTo: "/collection/3",
   },
   {
     id: 4,
     name: "INFUSIONS & BLENDS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Rose_Green_Tea_1000x.jpg?v=1592641347",
-    linkTo: "/collection/infusions-blends",
+    linkTo: "/collection/4",
   },
   {
     id: 5,
     name: "WHITE TEAS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Silky_Silver_Needle_Leaf_1000x.jpg?v=1592641627",
-    linkTo: "/collection/white-tea",
+    linkTo: "/collection/5",
   },
   {
     id: 6,
     name: "GREEN TEAS",
     image:
       "https://cdn.shopify.com/s/files/1/0287/1620/4135/collections/Mountain_Mist_Leaf_1000x.jpg?v=1592641599",
-    linkTo: "/collection/green-tea",
+    linkTo: "/collection/6",
   },
 ];
