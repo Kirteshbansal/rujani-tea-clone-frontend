8bb30e5ca7270711488dced293e4e40c93aeec6f
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-24 17:18:45 +0530

    Add getAllProducts function

--- src/projectService.js
+++ src/projectService.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = "https://rujani-tea-clone.herokuapp.com/api/";
+
+export const getAllProducts = async () => {
+  try {
+    return await axios.get(`${BASE_URL}products`);
+  } catch (err) {
+    console.error(err);
+  }
+};
