754200303821197570497e30bc34f44cc1408423
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-24 22:04:19 +0530

    Add getProductsByCollection function

--- src/projectService.js
+++ src/projectService.js
@@ -9,3 +9,11 @@ export const getAllProducts = async () => {
     console.error(err);
   }
 };
+
+export const getProductsByCollection = async (id) => {
+  try {
+    return await axios.get(`${BASE_URL}category/${id}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
