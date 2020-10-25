271e2f03c4c01238bde776a344c0df54286f74aa
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-25 22:37:29 +0530

    Add Cart.js file with basic jsx for drawer

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/core";
 
-class Cart extends Component {
+class CartDrawer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <></>;
+    const { isOpen, onOpen, onClose } = this.props;
+    return (
+      <>
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
+          <DrawerOverlay />
+          <DrawerContent background="linear-gradient(to bottom, #efefef ,#f2f2f2)">
+            <DrawerCloseButton _focus={{ outline: "none" }} my={3} mr={2} />
+            <DrawerHeader></DrawerHeader>
+            <DrawerBody></DrawerBody>
+            <DrawerFooter></DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
+    );
   }
 }
 
-export default Cart;
+export default CartDrawer;
