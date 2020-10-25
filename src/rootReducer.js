f1b9b773c3e0b9e5b7d4c563cf0c7c80ee1d0525
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-25 16:29:43 +0530

    Add productSlice reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import productsSlice from "./products/productsSlice";
 import collectionSlice from "./collection/collectionSlice";
+import productSlice from "./product/productSlice";
 
 export default combineReducers({
   products: productsSlice,
   collectionProducts: collectionSlice,
+  product: productSlice,
 });
