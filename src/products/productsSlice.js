b604461a583e103fd403552bfa0c919206429049
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-25 19:34:22 +0530

    Add sortProducts in reducers

--- src/products/productsSlice.js
+++ src/products/productsSlice.js
@@ -5,20 +5,43 @@ import { getAllProducts } from "../projectService";
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
   async (thunkAPI) => {
-    const response = await getAllProducts();
-    return response.data;
+    try {
+      const response = await getAllProducts();
+      return response.data;
+    } catch (err) {
+      console.error(err);
+    }
   }
 );
 
 const productsSlice = createSlice({
   name: "products",
-  initialState: { products: [] },
-  reducers: {},
+  initialState: {
+    products: [],
+    sortOption: "Alphabetically, A-Z",
+    loading: false,
+    error: "",
+  },
+  reducers: {
+    sortProducts(state, action) {
+      state.sortOption = action.payload;
+    },
+  },
   extraReducers: {
+    [fetchProducts.pending]: (state) => {
+      state.loading = true;
+    },
     [fetchProducts.fulfilled]: (state, action) => {
       state.products = [...action.payload];
+      state.loading = false;
+    },
+    [fetchProducts.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
     },
   },
 });
+
+export const { sortProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
