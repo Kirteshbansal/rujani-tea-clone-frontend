76e401a29d5c828705b1e7c30400f18a09060bff
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-26 13:14:58 +0530

    Add pending & rejected state in extraReducers

--- src/collection/collectionSlice.js
+++ src/collection/collectionSlice.js
@@ -5,18 +5,30 @@ import { getProductsByCollection } from "../projectService";
 export const fetchProductsByCollection = createAsyncThunk(
   "collectionProducts/fetchProductsByCollection",
   async (collectionId) => {
-    const response = await getProductsByCollection(collectionId);
-    return response.data;
+    try {
+      const response = await getProductsByCollection(collectionId);
+      return response.data;
+    } catch (err) {
+      console.error(err);
+    }
   }
 );
 
 const collectionSlice = createSlice({
   name: "collectionProducts",
-  initialState: { collectionProducts: {} },
+  initialState: { collectionProducts: {}, loading: false, error: "" },
   reducers: {},
   extraReducers: {
+    [fetchProductsByCollection.pending]: (state) => {
+      state.loading = true;
+    },
     [fetchProductsByCollection.fulfilled]: (state, action) => {
       state.collectionProducts = { ...action.payload };
+      state.loading = false;
+    },
+    [fetchProductsByCollection.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
     },
   },
 });
