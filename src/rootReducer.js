7c6f0cbf4a154c0010d6e3c8293d872434e92155
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-26 13:59:18 +0530

    Add cartSlice reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import productsSlice from "./products/productsSlice";
 import collectionSlice from "./collection/collectionSlice";
 import productSlice from "./product/productSlice";
+import cartSlice from "./cart/cartSlice";
 
 export default combineReducers({
   products: productsSlice,
   collectionProducts: collectionSlice,
   product: productSlice,
+  cart: cartSlice,
 });
