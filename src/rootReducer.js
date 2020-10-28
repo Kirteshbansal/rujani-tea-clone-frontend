5ccb3f8a65795a8ea6b64bf63dbd182aa8979b00
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-28 12:02:34 +0530

    Add userSlice reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,10 +4,12 @@ import productsSlice from "./products/productsSlice";
 import collectionSlice from "./collection/collectionSlice";
 import productSlice from "./product/productSlice";
 import cartSlice from "./cart/cartSlice";
+import userSlice from "./auth/authSlice";
 
 export default combineReducers({
   products: productsSlice,
   collectionProducts: collectionSlice,
   product: productSlice,
   cart: cartSlice,
+  user: userSlice,
 });
