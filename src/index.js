46850fc990ce669e782e345a02bfed2b21fedae0
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-28 12:03:18 +0530

    Comment serviceWorker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ThemeProvider, theme, CSSReset } from "@chakra-ui/core";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
