070d5391b0684cb1839a41d9e746921a206e73e0
kirtesh bansal <bansalkirtesh2.kb@gmail.com>
2020-10-29 11:12:25 +0530

    Comment auth.js file content

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,38 +1,38 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
 
-import LogIn from "./LogIn";
-import { userAuthentication } from "./authSlice";
+// import LogIn from "./LogIn";
+// import { userAuthentication } from "./authSlice";
 
-export default function isAuth(ReceivedComponent) {
-  class AuthCheck extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
+// export default function isAuth(ReceivedComponent) {
+//   class AuthCheck extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {};
+//     }
 
-    componentDidMount() {
-      this.props.userAuthentication();
-    }
+//     componentDidMount() {
+//       this.props.userAuthentication();
+//     }
 
-    render() {
-      return <>{this.props.loginSuccess ? <ReceivedComponent /> : <LogIn />}</>;
-    }
-  }
+//     render() {
+//       return <>{this.props.loginSuccess ? <ReceivedComponent /> : <LogIn />}</>;
+//     }
+//   }
 
-  const mapStateToProps = (state) => {
-    // console.log("auth file", state);
-    return {
-      user: { ...state.user.user },
-      loginSuccess: state.user.loginSuccess,
-    };
-  };
+//   const mapStateToProps = (state) => {
+//     // console.log("auth file", state);
+//     return {
+//       user: { ...state.user.user },
+//       loginSuccess: state.user.loginSuccess,
+//     };
+//   };
 
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      userAuthentication: () => dispatch(userAuthentication()),
-    };
-  };
+//   const mapDispatchToProps = (dispatch) => {
+//     return {
+//       userAuthentication: () => dispatch(userAuthentication()),
+//     };
+//   };
 
-  return connect(mapStateToProps, mapDispatchToProps)(AuthCheck);
-}
+//   return connect(mapStateToProps, mapDispatchToProps)(AuthCheck);
+// }
