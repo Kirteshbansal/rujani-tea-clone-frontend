320f716df2a77b96af61e968ce8bc6e9f9ef10ba
Kirtesh Bansal <bansalkirtesh2.kb@gmail.com>
2021-05-08 13:21:50 +0530

    Add routes.js

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+const routes = {
+    home: "/",
+    products: "/products",
+    category: "/collection/:id",
+    product: "/product/:id",
+    login: "/login",
+    signup: "/account/register",
+    checkout: "/checkout",
+    profile: "/profile",
+    about: "/about",
+    faq: "/faq",
+    teagram: "/blogs/the-teagram",
+    contact: "/contact-us",
+};
+
+export default routes;
